8738949351043547e140e7b4253a857732b744a9
ktaletski <kirill@taletski.com>
2023-08-10 22:22:11 +0300

    Add examples for the Set slide

--- Lesson-5-55-Aug-10/demo.js
+++ Lesson-5-55-Aug-10/demo.js
@@ -671,3 +671,23 @@ console.log(sum(2, 5, 5)); // 12
 
 const args = [2, 5, 5];
 console.log(sum(...args));
+
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// считаем гостей, некоторые приходят несколько раз
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set хранит только 3 уникальных значения
+alert(set.size); // 3
+
+for (let user of set) {
+  alert(user.name); // John (потом Pete и Mary)
+}
